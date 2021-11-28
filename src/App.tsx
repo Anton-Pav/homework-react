f18ff87f0b9c5c5f5a916b61ebe676f836fc5fac
Anton-Pav <ant.pavlov93@yandex.ru>
2021-11-28 19:26:47 +0300

    add useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,27 @@ import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import OnOff from "./components/OnOff/OnOff";
+import Uncontrolaccordion from './components/SelfcontrolledAccordion/Uncontrolaccordion';
 
 function App() {
     return (
         <div>
-            <PageTitle title={"This is APP components"}/>
+            <Uncontrolaccordion titleValue={"Menu"} />
+            <Uncontrolaccordion titleValue={"Users"} />
+            {/*<OnOff/>*/}
+            {/*<OnOff on={false}/>
+            <OnOff on={true}/>*/}
+           {/* <PageTitle title={"This is APP components"}/>
             <Rating value={3}/>
-            <Accordion titleValue={"Menu"} collapsed={true}/>
-            <Accordion titleValue={"Users"} collapsed={false}/>
+            <Uncontrolaccordion titleValue={"Menu"} collapsed={true}/>
+            <Uncontrolaccordion titleValue={"Users"} collapsed={false}/>
             <Rating value={0}/>
             <Rating value={1}/>
             <Rating value={2}/>
             <Rating value={3}/>
             <Rating value={4}/>
-            <Rating value={5}/>
+            <Rating value={5}/>*/}
         </div>
 
     );
